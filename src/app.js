3b2c8a97e864c755440e382836b4a5f66c45e394
booktrawler <67999133+booktrawler@users.noreply.github.com>
2020-07-27 17:41:28 +0100

    New Algolio Search page

---
+++ src/app.js
@@ -0,0 +1,41 @@
+/* global instantsearch algoliasearch */
+
+const search = instantsearch({
+  indexName: 'prod_Booklist',
+  searchClient: algoliasearch('958JH2XUYH', '3566d6f58734e0b0ff38a49d73486daa'),
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#brand-list',
+    attribute: 'brand',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <div>
+          <img src="{{image}}" align="left" alt="{{name}}" />
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-description">
+            {{#helpers.highlight}}{ "attribute": "category" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-price">\${{price}}</div>
+        </div>
+      `,
+    },
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();

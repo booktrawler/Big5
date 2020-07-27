ef34afee1ca1625c735f779f4913f19b189a0394
booktrawler <67999133+booktrawler@users.noreply.github.com>
2020-07-27 19:09:21 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ search.addWidgets([
     templates: {
       item: `
         <div>
-          <!-- <img src="{{image}}" align="left" width="50" alt="{{name}}" /> -->
           <div class="hit-name">
             <a href="{{link}}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a>
           </div>
